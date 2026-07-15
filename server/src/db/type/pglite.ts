2a1fd21f0144422ab9bfd2ba148d2d027d75eb35
Amanbig <amanpreetsinghjhiwant@outlook.com>
2026-07-15 17:26:49 +0530

    chore: db migration setup update

--- server/src/db/type/pglite.ts
+++ server/src/db/type/pglite.ts
@@ -2,7 +2,6 @@ import { PGlite } from '@electric-sql/pglite';
 import { drizzle } from 'drizzle-orm/pglite';
 import config from "../../config/config.js"
 
-// In-memory Postgres
 const client = new PGlite(config.DB_DIR);
 
 export const pglite_db = drizzle({ client });
